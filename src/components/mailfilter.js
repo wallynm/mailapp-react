1b37150db823e1548b7937ef39587735f97cec92
Wallyson <wallysson.mota@hotmart.com.br>
2017-03-12 00:19:56 -0300

    reindent fixed

--- src/components/mailfilter.js
+++ src/components/mailfilter.js
@@ -2,68 +2,68 @@ import React, { Component } from 'react';
 import Button from './button';
 
 export default class MailFilter extends Component {
-    render() {
-        return (
-            <div className="col-1 panel sidebar">
-                <header>
-                    <Button text="New message"/>
-                </header>
+  render() {
+    return (
+      <div className="col-1 panel sidebar">
+        <header>
+          <Button text="New message"/>
+        </header>
 
-                <ul className="mail-status">
-                    <li className="active">
-                        <span className="name">Inbox</span>
-                        <span className="counter">34</span>
-                    </li>
-                    <li>
-                        <span className="name">Sent</span>
-                        <span className="counter"></span>
-                    </li>
-                    <li>
-                        <span className="name">Draft</span>
-                        <span className="counter"></span>
-                    </li>
-                    <li>
-                        <span className="name">Junk</span>
-                        <span className="counter"></span>
-                    </li>
-                    <li>
-                        <span className="name">Trash</span>
-                        <span className="counter"></span>
-                    </li>
-                    <li>
-                        <span className="name">Reminder</span>
-                        <span className="counter"></span>
-                    </li>
-                </ul>
+        <ul className="mail-status">
+          <li className="active">
+            <span className="name">Inbox</span>
+            <span className="counter">34</span>
+          </li>
+          <li>
+            <span className="name">Sent</span>
+            <span className="counter"></span>
+          </li>
+          <li>
+            <span className="name">Draft</span>
+            <span className="counter"></span>
+          </li>
+          <li>
+            <span className="name">Junk</span>
+            <span className="counter"></span>
+          </li>
+          <li>
+            <span className="name">Trash</span>
+            <span className="counter"></span>
+          </li>
+          <li>
+            <span className="name">Reminder</span>
+            <span className="counter"></span>
+          </li>
+        </ul>
 
-                <ul className="mail-status">
-                    <label>Labels</label>
-                    <li>
-                        <i className="icon-circle color-orange"></i>
-                        <span className="name">Personal</span>
-                        <span className="counter">10</span>
-                    </li>
-                    <li>
-                        <i className="icon-circle color-green"></i>
-                        <span className="name">Work</span>
-                        <span className="counter">10</span>
-                    </li>
-                    <li>
-                        <i className="icon-circle color-purple"></i>
-                        <span className="name">Friends</span>
-                        <span className="counter">10</span>
-                    </li>
-                    <li>
-                        <i className="icon-circle color-blue"></i>
-                        <span className="name">Personal</span>
-                        <span className="counter">10</span>
-                    </li>
-                    <li>
-                        <i className="icon-add"></i>
-                        <span className="name">Add</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+        <ul className="mail-status">
+          <label>Labels</label>
+          <li>
+            <i className="icon-circle color-orange"></i>
+            <span className="name">Personal</span>
+            <span className="counter">10</span>
+          </li>
+          <li>
+            <i className="icon-circle color-green"></i>
+            <span className="name">Work</span>
+            <span className="counter">10</span>
+          </li>
+          <li>
+            <i className="icon-circle color-purple"></i>
+            <span className="name">Friends</span>
+            <span className="counter">10</span>
+          </li>
+          <li>
+            <i className="icon-circle color-blue"></i>
+            <span className="name">Personal</span>
+            <span className="counter">10</span>
+          </li>
+          <li>
+            <i className="icon-add"></i>
+            <span className="name">Add</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
--- src/components/maillist.js
+++ src/components/maillist.js
@@ -2,118 +2,114 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 
 class MailListItem extends Component {
-   constructor(props) {
-        super(props);
-        this.state = {
-            new: props.item.new
-        };
-        this.select = this.select.bind(this);
-    }
-    
-
-    select(){
-        this.setState({new: false});
-        this.props.select(this.props.item.id);
-    }
-
-    render() {
-        return(
-            <li className={`label-color-${this.props.tag} ` + ((this.props.selected === this.props.item.id) ? 'active' : '')} 
-                onClick={this.select}>
-                {this.state.new === true &&
-                <div className="status-icon">
-                    <i className="icon-circle-fill color-blue"/>
-                </div>
-                }
-                <div className="name">{this.props.item.name}</div>
-                <div className="title">{this.props.item.title}</div>
-                <div className="preview">{this.props.item.preview}...</div>
-                <time>{this.props.date}</time>
-            </li>
-        )
-    }
+ constructor(props) {
+  super(props);
+  this.state = {
+    new: props.item.new
+  };
+  this.select = this.select.bind(this);
 }
 
+select(){
+  this.setState({new: false});
+  this.props.select(this.props.item.id);
+}
+
+render() {
+  return(
+    <li className={`label-color-${this.props.tag} ` + ((this.props.selected === this.props.item.id) ? 'active' : '')} onClick={this.select}>
+    {this.state.new === true &&
+      <div className="status-icon">
+        <i className="icon-circle-fill color-blue"/>
+      </div>
+    }
+    <div className="name">{this.props.item.name}</div>
+    <div className="title">{this.props.item.title}</div>
+    <div className="preview">{this.props.item.preview}...</div>
+    <time>{this.props.date}</time>
+    </li>
+    )
+  }
+}
 
 class MailList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: 1
-        };
-        this.select = this.select.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 1
+    };
+    this.select = this.select.bind(this);
+  }
 
+  select(itemID) {
+    this.setState({ selected: itemID});
+  }
 
-    select(itemID) {
-        this.setState({ selected: itemID});
-    }
+  render() {
+    let data = [{
+      id: 1,
+      name: "Lisa Guerrero",
+      title: "Company Goals for 2016",
+      preview: "Hello everyone, i'm happy to share with you our new company goals",
+      date: "20:24",
+      tag: "green",
+      new: true
+    }, {
+      id: 2,
+      name:"Lisa Guerrero",
+      title:"Company Goals for 2016",
+      preview:"Hello everyone, i'm happy to share with you our new company goals",
+      date: "20:24",
+      tag: "purple",
+      new: true
+    }, {
+      id: 3,
+      name: "Peter Gregor",
+      title: "Design for health project",
+      preview: "Hi Jessica, I love your UI design work, and i'd like to talk with you",
+      date: "14:10",
+      tag: "green",
+      new: true
+    }, {
+      id: 4,
+      name: "Sara Richardson",
+      title: "Meeting Zurich",
+      preview: "Hi Jessica, I will be in Zurich tomorrow, hope we can meet there",
+      date: "17:10",
+      tag: "blue",
+      new: true
+    }];
 
-    render() {
-        let data = [{
-            id: 1,
-            name: "Lisa Guerrero",
-            title: "Company Goals for 2016",
-            preview: "Hello everyone, i'm happy to share with you our new company goals",
-            date: "20:24",
-            tag: "green",
-            new: true
-        }, {
-            id: 2,
-            name:"Lisa Guerrero",
-            title:"Company Goals for 2016",
-            preview:"Hello everyone, i'm happy to share with you our new company goals",
-            date: "20:24",
-            tag: "purple",
-            new: true
-        }, {
-            id: 3,
-            name: "Peter Gregor",
-            title: "Design for health project",
-            preview: "Hi Jessica, I love your UI design work, and i'd like to talk with you",
-            date: "14:10",
-            tag: "green",
-            new: true
-        }, {
-            id: 4,
-            name: "Sara Richardson",
-            title: "Meeting Zurich",
-            preview: "Hi Jessica, I will be in Zurich tomorrow, hope we can meet there",
-            date: "17:10",
-            tag: "blue",
-            new: true
-        }];
+    return (
+      <div className="col-2 panel list-panel">
+        <header>
+          <div className="dropdown-list">
+          <label htmlFor="select-sort">Sort by
+            <span className="selected-item">Date</span>
+            <i className="icon-caret-down"></i>
+          </label>
+          <input type="checkbox" id="select-sort"/>
+            <ul className="shadow-box">
+              <li><label htmlFor="select-sort">Order by date</label></li>
+              <li><label htmlFor="select-sort">Order by priority</label></li>
+              <li><label htmlFor="select-sort">Order by frienship</label></li>
+            </ul>
+          </div>
+        </header>
 
-        return (
-            <div className="col-2 panel list-panel">
-                <header>
-                    <div className="dropdown-list">
-                        <label htmlFor="select-sort">Sort by
-                            <span className="selected-item">Date</span>
-                            <i className="icon-caret-down"></i>
-                        </label>
-                        <input type="checkbox" id="select-sort"/>
-                        <ul className="shadow-box">
-                            <li><label htmlFor="select-sort">Order by date</label></li>
-                            <li><label htmlFor="select-sort">Order by priority</label></li>
-                            <li><label htmlFor="select-sort">Order by frienship</label></li>
-                        </ul>
-                    </div>
-                </header>
-
-                <ul className="mail-list">
-                    {_.map(data, (item, key) =>
-                        <MailListItem
-                            key={key}
-                            item={item}
-                            select={this.select}
-                            selected={this.state.selected}
-                        />
-                    )}
-                </ul>
-            </div>
-        );
-    }
+        <ul className="mail-list">
+          {_.map(data, (item, key) =>
+            <MailListItem
+            key={key}
+            item={item}
+            select={this.select}
+            selected={this.state.selected}
+            />
+          )}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default MailList;
